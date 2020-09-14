6439b0367ab721572d272300d0f18beadbd139e2
Lhimbo <32436536+Lhimbo@users.noreply.github.com>
2020-09-13 23:45:46 -0300

    Add files via upload

---
+++ webcrawler-zro-ichi/Anime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace webcrawler_zro_ichi
+{
+    [Table("anime")]
+    public class Anime 
+    {
+        public string title { get; set; }
+        public string link { get; set; }
+        public string img_src { get; set; }
+        public int id { get; set; }
+        public DateTime release_date { get; set; }
+
+        public Anime(string title, string img_src, string link,int id)
+        {
+            this.title = title;
+            this.link = link;
+            this.img_src = img_src;
+            this.id = id;
+        }
+    }
+}
---
+++ webcrawler-zro-ichi/AnimeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace webcrawler_zro_ichi
+{
+    public class AnimeContext : DbContext 
+    {
+        public AnimeContext() {}
+
+        public DbSet<Anime> animes { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+           => optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=postgree_tuts;Username=postgres;Password=exmachina0451");
+
+       
+    }
+}
